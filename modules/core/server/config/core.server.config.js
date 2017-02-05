db39bf805e8e48aed6107757fbf3a095dfee5c43
jcmangaliag <mangaliagj@gmail.com>
2017-02-06 02:13:51 +0800

    Created config and modified the server

---
+++ modules/core/server/config/core.server.config.js
@@ -0,0 +1,8 @@
+const config = {
+	name: 'PCAARRD KM Communities',
+	host: 'localhost',
+	port: process.env.PORT || 3000,
+	env: process.env.NODE_ENV || 'development'
+};
+
+export default config;
---
+++ modules/core/server/core.server.app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import config from './config/core.server.config';
+import moduleRoutes from './routes/core.server.routes';
+
+const app = express();
+app.use(bodyParser.json());
+
+moduleRoutes(app);
+
+app.all('/*', (req, res) => {
+	res.sendFile(path.join(`${__dirname}/../client/base-view/core.client.base-view.html`));
+});
+
+app.listen(config.port, () => {
+	console.log(`Server running on ${config.host}:${config.port}`);
+});
