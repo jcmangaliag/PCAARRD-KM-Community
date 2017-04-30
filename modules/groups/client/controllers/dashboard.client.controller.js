d326aa2732ac0e1a363339f5bb2000009f353c1a
jcmangaliag <mangaliagj@gmail.com>
2017-04-30 16:13:37 +0800

    Functional Top Active Groups in Community Dashboard

--- modules/groups/client/controllers/dashboard.client.controller.js
+++ modules/groups/client/controllers/dashboard.client.controller.js
@@ -22,7 +22,7 @@ import _ from 'lodash';
 				$scope.groups = results[0];
 				$scope.posts = results[1];
 				$scope.users = results[2];
-				
+
 				$scope.computeAnalytics();
 			}, (error) => {	// can't load all data
 				ngToast.create({
@@ -42,7 +42,58 @@ import _ from 'lodash';
 		}
 
 		$scope.computeTopActiveGroups = () => {
-			$scope.loadTopActiveGroups();
+			/** Getting the top active groups of current month **/
+
+			// retrieve all posts of the current month and year
+			let currentMonthPosts = $scope.posts.filter((post) =>
+				(moment(post.datePosted, 'MMMM Do YYYY, h:mm:ss a').format('M') == moment().month()+1) && 
+				(moment(post.datePosted, 'MMMM Do YYYY, h:mm:ss a').format('YYYY') == moment().year())
+			);
+			
+			// create object containing group handles as key and the value is the count of posts from the currentMonthPosts
+			let postsGroupsWithCount = {};
+			_.forEach(currentMonthPosts, (post) => {
+				if (postsGroupsWithCount.hasOwnProperty(post.groupBelonged)){
+					postsGroupsWithCount[post.groupBelonged]++;
+				} else {
+					postsGroupsWithCount[post.groupBelonged] = 1;
+				}
+			});
+
+			// create an array containing the groups sorted by post counts in reverse
+			let sortedGroupsArray =  _(postsGroupsWithCount).keys().sort((key1, key2) => postsGroupsWithCount[key2] - postsGroupsWithCount[key1]).map((key) => {
+				return key;
+			}).value();
+			
+			// limit the array size to at most 5
+			const topActiveGroupsSize = sortedGroupsArray.length < 5? sortedGroupsArray.length : 5;
+			sortedGroupsArray = sortedGroupsArray.slice(0, topActiveGroupsSize);
+			
+
+			/** Getting the count of posts of the top active groups for the previous months of the current year **/
+
+			// in each group in the top active, retrieve the count of posts for the months of this year
+			const consideredMonths = moment.months().slice(0, moment().month()+1);
+			let topActiveGroups = [];
+
+			_.forEach(sortedGroupsArray, (group) => {
+				let groupPostsCountList = [];
+
+				_.forEach(consideredMonths, (month) => {
+
+					const postsCount = $scope.posts.filter((post) => 
+						(moment(post.datePosted, 'MMMM Do YYYY, h:mm:ss a').format('MMMM') == month) && 
+						(moment(post.datePosted, 'MMMM Do YYYY, h:mm:ss a').format('YYYY') == moment().year()) &&
+						(post.groupBelonged === group)
+					).length;
+
+					groupPostsCountList.push(postsCount);
+				});
+
+				topActiveGroups.push({name: group, data: groupPostsCountList});
+			});
+
+			$scope.loadTopActiveGroups(topActiveGroups);
 		}
 
 		$scope.computeTrendingTopics = () => {
@@ -64,10 +115,10 @@ import _ from 'lodash';
 					.slice(0, TOP_COUNT);	// temporary
 				// top5ActiveGroups should scan posts and get the total for that month
 			top5ActiveGroupsSeries*/
-		$scope.loadTopActiveGroups = () => {
+		$scope.loadTopActiveGroups = (topActiveGroups) => {
 			Highcharts.chart('top-active-groups-container', {
 			    title: {
-			    	text: `Top 5 Active Groups Per Month for ${moment().year()}`
+			    	text: `Top Active Groups Per Month for ${moment().year()}`
 			    },
 			    subtitle: {
 			    	text: 'Source: PCAARRD KM Community'
@@ -88,22 +139,7 @@ import _ from 'lodash';
 	               align: 'right',
 	               verticalAlign: 'middle'
 			    },
-			    series: [{
-			    name: 'Banana',
-			    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-			    }, {
-			    name: 'Coconut',
-			    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-			    }, {
-			    name: 'Mangrove Crabbers',
-			    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-			    }, {
-			    name: 'Biodiversity',
-			    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-			    }, {
-			    name: 'Abaca',
-			    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-			    }]
+			    series: topActiveGroups
 		    });
 		}
 
@@ -181,7 +217,7 @@ import _ from 'lodash';
 			        type: 'column'
 			    },
 			    title: {
-			        text: 'Top 5 Popular Groups by Age Group'
+			        text: 'Top Popular Groups by Age Group'
 			    },
 			    subtitle: {
 			        text: 'Source: PCAARRD KM Community'
