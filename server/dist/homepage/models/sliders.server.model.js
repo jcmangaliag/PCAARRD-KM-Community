11d8dec7527bdc15e89b930ba43daf336fd462dd
Evan Tolorio <evantolorio@gmail.com>
2017-08-29 22:51:08 +0800

    Compile and build client and server assets

---
+++ server/dist/homepage/models/sliders.server.model.js
@@ -0,0 +1,2 @@
+'use strict';Object.defineProperty(exports,'__esModule',{value:!0});var _mongoose=require('mongoose'),_mongoose2=_interopRequireDefault(_mongoose);function _interopRequireDefault(a){return a&&a.__esModule?a:{default:a}}var SliderSchema=new _mongoose2.default.Schema({backgroundImage:{type:String,required:!0},caption:{title:String,description:String,actionButton:{shouldUse:Boolean,text:String,sref:String}},order:{type:Number,required:!0}});exports.default=_mongoose2.default.model('Slider',SliderSchema);
+//# sourceMappingURL=sliders.server.model.js.map
